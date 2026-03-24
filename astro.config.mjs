32003abf7e952756c3f14bcc4abcc9145940bb43
sektor30 <sektor9130@gmail.com>
2026-03-24 10:17:17 -0500

    new add gh pages

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+// Modo 'static' para generación estática de todas las páginas
+export default defineConfig({
+  output: 'static',
+  site: 'https://satelite30.github.io',
+  base: '/blog-pop-dos-astro-dev/'
+});
