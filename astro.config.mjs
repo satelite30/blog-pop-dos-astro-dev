bfa83ba0ac1e99bdfd5e7a47c85d593f75476fb6
satalite30 <sergioudea91>
2026-03-25 11:38:58 -0500

    changes og setting 3 seo integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 
 // https://astro.build/config
 // Modo 'static' para generación estática de todas las páginas
 export default defineConfig({
   output: 'static',
-  site: 'https://blog.popular-esmedellin.com'
+  site: 'https://blog.popular-esmedellin.com',
+  trailingSlash: 'always',
+
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ]
+
   
 });
 
