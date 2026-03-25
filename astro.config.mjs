34e8b770ec25d642dac0ef0bd36ad7816baaf06d
sektor30 <sektor9130@gmail.com>
2026-03-25 08:48:19 -0500

    pre merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 // Modo 'static' para generación estática de todas las páginas
 export default defineConfig({
